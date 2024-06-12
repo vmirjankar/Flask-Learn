014771ef11c8e64c520569580e248cfe6ca1799a
Ved Mirjankar <111427005+vmirjankar@users.noreply.github.com>
2024-06-12 11:21:11 +0530

    Add files via upload

---
+++ tutorial1.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for
+
+app=Flask(__name__)
+
+@app.route("/")
+#home page
+def home():
+	return "Hello! This is the main page <h1>HELLO<h1>"
+
+@app.route("/<name>") #passes the text as parameter to the user func
+def user(name):
+	return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+	return redirect(url_for("home")) #redirects to the specified function
+
+if __name__=="__main__":
+	#run app
+	app.run()
